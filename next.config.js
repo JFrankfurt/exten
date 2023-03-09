788e49b8013135b4a8d8107957db364d04715c06
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-03-09 13:43:51 -0600

    the beef

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
